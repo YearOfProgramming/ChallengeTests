f9af1d9db71ed7bc94bc15a6cc85f054502e2ad5
Steven Landau <skl8881@g.rit.edu>
2017-01-12 13:03:25 -0500

    Setting up the circular list

--- challenge_15/java/slandau3/TakingCandyFromABaby.java
+++ challenge_15/java/slandau3/TakingCandyFromABaby.java
@@ -1,8 +1,8 @@
 
 
 public class TakingCandyFromABaby<T> {
-    public int size = 0;
-    public Node current = null;
+    private int size = 0;
+    public Node root = null;
 
     private class Node {
         public Node next = null;
@@ -13,4 +13,39 @@ public class TakingCandyFromABaby<T> {
         }
     }
 
+    private void incrementSize() {
+        this.size++;
+    }
+
+    private void decrementSize() {
+        this.size--;
+    }
+
+    public void append(T data) {
+        if (size == 0) {
+            this.root = new Node(data);
+        } else {
+            Node temp = this.root;
+            int cur = 0;
+            while (cur != size) {
+                temp = temp.next;
+            }
+            temp.next = new Node(data);
+            temp.next.next = this.root;
+        }
+        incrementSize();
+    }
+
+    public void printList() {
+        Node temp = this.root;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public T removeNode(T data) {
+        }
+        
+    }
 }
